153dec16074371d12d0b8fccf96df8d1200c8900
Zonaro <zonaro@outlook.com>
2022-09-22 11:10:01 -0300

    -

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -161,6 +161,7 @@ const main = (async function () {
 	window.basePath = getParam('basePath') || '';
 
 	if (window.repo.isBlank() && window.basePath.isBlank()) {
+		debugger;
 		let hash = location.hash.substring(1);
 		if (hash.isNotBlank() && hash.startsWith('/')) {
 			let parts = hash.split('#');
