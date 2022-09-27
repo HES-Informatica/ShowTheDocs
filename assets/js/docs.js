4934ed3e2d278f4a6c9996c9bd9830e0b9462030
Zonaro <zonaro@outlook.com>
2022-09-27 09:07:21 -0300

    -

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -185,13 +185,11 @@ const main = (async function () {
 	if (window.repo.isNotBlank()) {
 		debugger;
 		let parts = window.repo.split("/").filter((x) => x.isNotBlank() && x != "#");
-		switch (parts.length) {
-			case 3:
-				//do nothing, its okay	
-				break;
+		switch (parts.length) {			
 			case 2:
 				parts.push("main");
-			case 1:
+				break;
+			case 1:			
 				parts = ["zonaro", "showTheDocs", "main"];
 			default:
 				parts = parts.slice(0, 3);
